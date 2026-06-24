6d1b0c9a17ff02671c876db443d66478dadcf7a3
Gabriel Andrade <107321330+imgabrie1@users.noreply.github.com>
2026-06-24 14:58:03 -0300

    fix: add sao paulo timezone (#61)

--- src/pages/app/Dashboard/AppointmentSummary/index.tsx
+++ src/pages/app/Dashboard/AppointmentSummary/index.tsx
@@ -31,6 +31,15 @@ const AppointmentSummaryPage = () => {
     navigate("/app");
   };
 
+  const formatToLocalTimeString = (date: Date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    return `${year}-${month}-${day}T${hours}:${minutes}:00`;
+  };
+
   const handleConfirmAppointment = async () => {
     if (selectedAppointmentItems.length === 0) return;
     if (isPending) return;
@@ -38,7 +47,7 @@ const AppointmentSummaryPage = () => {
     try {
       for (const item of selectedAppointmentItems) {
         await mutateAsync({
-          startTime: item.dateTime.toISOString(),
+          startTime: formatToLocalTimeString(item.dateTime),
           barberId: item.barberId,
           serviceIds: [item.serviceId],
           shopId: item.shopId,
@@ -73,6 +82,7 @@ const AppointmentSummaryPage = () => {
       day: "numeric",
       hour: "2-digit",
       minute: "2-digit",
+      timeZone: "America/Sao_Paulo",
     });
   };
 
