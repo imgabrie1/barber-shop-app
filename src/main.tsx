6c161932085dcab869200390902c95f702c75c64
Gabriel Andrade <gabrielcoloradogvda@gmail.com>
2026-07-09 16:04:12 -0300

    feat(analytics): add vercel web analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
+// main.tsx
 import "./styles/global.css"
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/global.css";
 import App from "./App";
+import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>
 );
